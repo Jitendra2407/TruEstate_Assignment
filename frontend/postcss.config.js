fa258b88b98ca27bd66cf74291f6a0763259dec4
Jitendra2407 <jitendrakumarthakur552@gmail.com>
2025-12-06 23:19:46 +0530

    css error

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,13 +1,6 @@
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default {
   plugins: {
-    tailwindcss: {
-      config: join(__dirname, 'tailwind.config.js'),
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
